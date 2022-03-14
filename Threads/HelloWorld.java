5189446cd05cc37f2a2927f0ee49d30b5d816dca
VenkataJaswanth <85697901+VenkataJaswanth@users.noreply.github.com>
2022-03-14 10:54:17 +0530

    Create HelloWorld.java

---
+++ Threads/HelloWorld.java
@@ -0,0 +1,15 @@
+//Creating Five threads and printing Hello World
+class Main extends Thread {
+	public void run()
+	{
+		System.out.println(Thread.currentThread().getName() +" Hello World");
+	}
+    public static void main(String[] args)
+	{
+		int n = 5; 
+		for (int i = 0; i < n; i++) {
+			Main thread= new Main();
+			thread.start();
+		}
+	}	
+}
